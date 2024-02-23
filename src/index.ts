f630d03fce8da5d4d65aff8f85b8a2794c6b3862
Hakan Akın <cehakanakin@gmail.com>
2024-02-23 03:31:05 +0300

    added running solution method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import { exampleFunction } from './example';
+import getData from './services/getData';
+import { buildPackages, connectInvoice, produceRestockingList, throwWarnings } from './utils';
 
-console.log(exampleFunction());
+// Main function to run the solution
+const runSolution = async () => {
+  const { orders, heatPumps, installationMaterials, tools } = await getData();
+
+  const products = [...heatPumps, ...installationMaterials, ...tools];
+  const packages = buildPackages(orders, products);
+  const invoice = connectInvoice(packages);
+  const restockList = produceRestockingList(products);
+  throwWarnings(orders, products);
+
+  console.log('Invoice:', invoice);
+  console.log('Restocking List:', restockList);
+};
+
+// Run the solution
+runSolution();
